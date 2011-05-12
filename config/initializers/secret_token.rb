5a2b345e13192ece427c1179c69e86e0bc3f7bcc
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-05-12 15:40:20 -0700

    expire old sessions

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Scrummer::Application.config.secret_token = '9cbc61ae510d1e670a9f7c4d00805bebc88a740be68b2794cde07d7438d68ff0265cb4ec10074fb5d0bb0495902c0de05c2ed060fb817a2d29c13aecacd1c181'
+Scrummer::Application.config.secret_token = '20b5954b157107689f48140389518f7d2e77540b3434961c4688d5834cd529acf857328bf60e2a5ff9520669cdbfd60ab13190ad8602e8ee670fe40d1a4c001a'
